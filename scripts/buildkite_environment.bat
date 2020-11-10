1315b6de1ae8cf2c0d0ccb2d884057d6e750c783
thuynt <thuynt@bit-vietnam.com>
2020-11-10 16:04:40 +0700

    no message

--- scripts/buildkite_environment.bat
+++ scripts/buildkite_environment.bat
@@ -1,7 +1,9 @@
 @ECHO OFF
 ECHO "---Setting up the environment"
 SET VERCEL_TOKEN=5nRQpWoeqRyIATa7QreqsI79cd
-IF $BUILDKITE_BRANCH == "master"
-  SET NODE_ENV=production
+IF [ $BUILDKITE_BRANCH -eq "master"]
+  THEN
+    SET NODE_ENV=production
 ELSE
   SET NODE_ENV=test
+FI
